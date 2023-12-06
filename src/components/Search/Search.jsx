1ca313724a1002bfc0b0861c86130214f19066c4
VishalArora14 <poojaarora5082@gmail.com>
2023-12-06 13:50:11 +0530

    removed eczyma from search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './search.css'; // Import your CSS file for styling
+import './search.css';
 
 const diseasesData = [
   {
@@ -26,37 +26,9 @@ const diseasesData = [
         description: 'Guttate psoriasis is a skin disease that often appears without warning and usually follows an infection like strep throat. It’s most common in young adults but can also happen in adults. The hallmark symptom of this condition is breaking out in small, red scaly patches...',
         moreInfo: 'https://my.clevelandclinic.org/health/diseases/22812-guttate-psoriasis',
       },
-      // Add more subtypes for Psoriasis as needed
     ],
   },
-  {
-    name: 'Eczyma',
-    subtypes: [
-      {
-        id: 1,
-        name: 'India',
-        image: 'plaque_pso.JPEG',
-        description: 'The most common type of psoriasis, plaque psoriasis causes dry, itchy, raised skin patches (plaques) covered with scales. There may be few or many. They usually appear on the elbows, knees, lower back and scalp. The patches vary in color, depending on skin color. The affected skin might heal with temporary changes in color (post inflammatory hyperpigmentation), particularly on brown or Black skin...',
-        moreInfo: 'https://www.mayoclinic.org/diseases-conditions/psoriasis/symptoms-causes/syc-20355840#:~:text=Plaque%20psoriasis.,color%2C%20depending%20on%20skin%20color.',
-      },
-      {
-        id: 2,
-        name: 'Australia',
-        image: 'nail_pso.jpg',
-        description: 'Most people develop nail psoriasis years after psoriasis begins. It’s also possible to have nail psoriasis without signs of psoriasis on your skin...',
-        moreInfo: 'https://www.aad.org/public/diseases/psoriasis/treatment/genitals/nails',
-      },
-      {
-        id: 3,
-        name: 'Canada',
-        image: 'Guttate_pso.JPEG',
-        description: 'Guttate psoriasis is a skin disease that often appears without warning and usually follows an infection like strep throat. It’s most common in young adults but can also happen in adults. The hallmark symptom of this condition is breaking out in small, red scaly patches...',
-        moreInfo: 'https://my.clevelandclinic.org/health/diseases/22812-guttate-psoriasis',
-      },
-      // Add more subtypes for Psoriasis as needed
-    ],
-  },
-  // Add more disease data as needed
+  // Add more disease data 
 ];
 
 const SearchPage = () => {
--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,2 +1,7 @@
 export const BASE_URL = "http://localhost:8000/predict";
+
+//flask
 // export const BASE_URL = "http://34.172.139.76/predict";
+
+//fast api
+// export const BASE_URL = "http://130.211.121.184/predict";
