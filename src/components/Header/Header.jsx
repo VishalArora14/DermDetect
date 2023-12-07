b40c3d71b03fe6c9dcb15dcfc202b1e9403bf62b
VishalArora14 <poojaarora5082@gmail.com>
2023-12-07 11:58:12 +0530

    made home page responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import "./header.css"
+import "./header.css";
 
 const Header = () => {
   return (
-      <div className="headerCont">
+    <div className="headerCont">
       <span className="logoImg"></span>
       <div className="context"> DermDetect </div>
       <div className="navBar">
@@ -14,12 +14,9 @@ const Header = () => {
         <Link to="/search">
           <div className="headerBtn">Search</div>
         </Link>
-        {/* <Link to="/about">
-          <div className="headerBtn">About</div>
-        </Link> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
